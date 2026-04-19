b96a02689d17807d202facd38c75a6ef8d87fc56
jeffersonreyes070-maker <jeffersonreyes070@gmail.com>
2026-04-18 20:35:15 -0600

    Update Usuarios.cs

--- Entities/Usuarios.cs
+++ Entities/Usuarios.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string Apellido { get; set; }
         public string Telefono { get; set; }
         public string Email { get; set; }
         public List<Reservas> Reservas { get; set; } = null!;
