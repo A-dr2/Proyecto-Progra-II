eacc9cb356c1918df99bdcb50db0d71decc795b9
A-dr2 <nael.barrantes.diaz@est.una.ac.cr>
2026-04-18 20:23:16 -0600

    Add CRUD methods to IListaDeEsperaServices interface

--- Services/Interfaces/IListaDeEsperaSerives.cs
+++ Services/Interfaces/IListaDeEsperaSerives.cs
@@ -5,6 +5,9 @@ namespace Proyecto_Progra_II.Services.Interfaces
     public interface IListaDeEsperaSerives
     {
         public List<ListaDeEspera> GetListaDeEspera();
-
+       public ListaDeEspera GetListaDeEsperaById(int id);  
+       public ListaDeEspera CreateListaDeEspera(ListaDeEspera listaDeEspera);
+       public ListaDeEspera UpdateListaDeEspera(int id, ListaDeEspera listaDeEspera);
+       public void DeleteListaDeEspera(int id);
     }
 }
