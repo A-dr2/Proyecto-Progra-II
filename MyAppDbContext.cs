4c6bf6de2339b24853dee87771a2e9443d4c512a
Daniel Acuña <acunamoradaniel10@gmail.com>
2026-04-18 21:48:51 -0600

    Configura EF Core InMemory y limpia código de ejemplo
    - Se eliminan WeatherForecastController y WeatherForecast.
    - Se agrega MyAppDbContext con AppDbContext para EF Core.
    - Se configura la base de datos en memoria en Program.cs.
    - Se corrige inicialización de Bloqueos en Mesas.
    - Se elimina MiDbContext y se añade el paquete EFCore.InMemory.
    - Se ajustan carpetas en el archivo de proyecto.

---
+++ MyAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto_Progra_II
+{
+    public class MyAppDbContext
+    {
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options)
+                : base(options) { }
+
+            // Colorcar esto para cada una de las entidades public DbSet<Entidad> Entidad { get; set; }
+        }
+    }
+}
