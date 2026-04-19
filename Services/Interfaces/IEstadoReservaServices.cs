30c4563468533ac0fa270c2667199f3788d5e0b7
NAEL BARRANTES DIAZ <nael.barrantes.diaz@est.una.ac.cr>
2026-04-18 20:10:31 -0600

    Actualización

--- Services/Interfaces/IEstadoReservaServices.cs
+++ Services/Interfaces/IEstadoReservaServices.cs
@@ -1,6 +1,18 @@
-﻿namespace Proyecto_Progra_II.Services.Interfaces
+﻿using Proyecto_Progra_II.Entities;
+
+namespace Proyecto_Progra_II.Services.Interfaces
 {
     public interface IEstadoReservaServices
     {
+        public List<EstadoReserva> GetAllEstadoReserva(); 
+        public EstadoReserva GetEstadoReservaId(int id);
+        public EstadoReserva CreateEstadoReserva(EstadoReserva estadoReserva);
+
+        public EstadoReserva UpdateEstadoReserva(int id, EstadoReserva estadoReserva);
+
+        public void DeleteEstadoReserva(int id);
+
+
+
     }
 }
--- Services/Interfaces/IZonasServices.cs
+++ Services/Interfaces/IZonasServices.cs
@@ -1,6 +1,13 @@
-﻿namespace Proyecto_Progra_II.Services.Interfaces
+﻿using Proyecto_Progra_II.Entities;
+
+namespace Proyecto_Progra_II.Services.Interfaces
 {
     public interface IZonasServices
     {
+        public List<Zonas> GetZonas();
+        public Zonas GetZonaById(int id);
+        public Zonas CreateZona(Zonas zona);
+        public Zonas UpdateZona(int id, Zonas zona);
+        public void DeleteZona(int id);
     }
 }
