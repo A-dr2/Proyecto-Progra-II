c8552fb98342298da9ecbd64b41705fb98d490ac
A-dr2 <nael.barrantes.diaz@est.una.ac.cr>
2026-05-01 16:33:33 -0600

    Delete Entities directory

--- Entities/EstadoReserva.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Proyecto_Progra_II.Entities
-{
-    public class EstadoReserva
-    {
-        public int Id { get; set; }
-        public required string Nombre { get; set; }
-    }
-}
--- Entities/Horarios.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Proyecto_Progra_II.Entities
-{
-    public class Horarios
-    {
-        public int Id { get; set; }
-        public TimeSpan HoraInicio { get; set; }
-        public TimeSpan HoraFin { get; set; }
-    }
-}
